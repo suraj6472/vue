28c68866427f56a8e5fc08658fa3619553c5af54
Suraj Kumar <surajk@capitalnumbers.com>
2023-09-19 19:07:17 +0530

    fetch data from store via computed property

--- src/components/ProductListTwo.vue
+++ src/components/ProductListTwo.vue
@@ -2,7 +2,7 @@
   <div id="product-list-two">
     <h2>Product List Two</h2>
     <ul>
-      <li v-for="product, index in products" :key="index">
+      <li v-for="(product, index) in products" :key="index">
         <span class="name">{{ product.name }}</span>
         <span class="price">£{{ product.price }}</span>
       </li>
@@ -12,9 +12,10 @@
 
 <script>
 export default {
-  props: ["products"],
-  data() {
-    return {};
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
   },
 };
 </script>
